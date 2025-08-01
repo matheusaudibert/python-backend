ed6cc78008e5a9e475981eb3f8207fa10d6b3421
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-08-01 15:44:11 -0300

    Aula 3

--- app.py
+++ app.py
@@ -19,20 +19,31 @@ def exibir_opcoes():
 def finalizar_app():
   os.system("clear")
   print("Finalizando o app\n")
+  
+def opcao_invalida():
+  print("Opção inválida!")
+  input("Digite uma tecla para voltar ao menu principal")
+  main()
 
 def escolher_opcoes():
-  opcao_escolhida = int(input("Escolha uma opção: "))
-  print(f"Você escolheu a opção {opcao_escolhida}")
-  if (opcao_escolhida == 1):
-    print("Cadastrar Restaurante")
-  elif(opcao_escolhida == 2):
-    print("Cadastrar Restaurante")
-  elif(opcao_escolhida == 3):
-    print("Ativar Restaurante")
-  else:
-    finalizar_app()
+  try:
+    opcao_escolhida = int(input("Escolha uma opção: "))
+    print(f"Você escolheu a opção {opcao_escolhida}")
+    if (opcao_escolhida == 1):
+      print("Cadastrar Restaurante")
+    elif(opcao_escolhida == 2):
+      print("Cadastrar Restaurante")
+    elif(opcao_escolhida == 3):
+      print("Ativar Restaurante")
+    elif(opcao_escolhida == 4):
+      finalizar_app()
+    else:
+      opcao_invalida()
+  except ValueError:
+    opcao_invalida()
 
 def main():
+  os.system("clear")
   exibir_nome_do_programa()
   exibir_opcoes()
   escolher_opcoes()
