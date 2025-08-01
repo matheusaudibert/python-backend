7079a5ee1af3a2eab2f947e5af264dda15398e12
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-08-01 16:15:12 -0300

    Aula 3

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ["Pizza", "Sushi"]
+
 def exibir_nome_do_programa():
   print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -14,7 +16,7 @@ def exibir_opcoes():
   print("1. Cadastrar Restaurante")
   print("2. Listar Restaurantes")
   print("3. Ativar Restaurantes")
-  print("4. Sair")
+  print("4. Sair\n")
   
 def finalizar_app():
   os.system("clear")
@@ -22,24 +24,44 @@ def finalizar_app():
   
 def opcao_invalida():
   print("Opção inválida!")
-  input("Digite uma tecla para voltar ao menu principal")
+  voltar_ao_menu_principal()
+  
+def exibir_subtitulo(texto):
+  os.system("clear")
+  print(f"{texto}\n")
+
+def voltar_ao_menu_principal():
+  input("\nDigite uma tecla para voltar ao menu principal")
   main()
+  
+def cadastra_novo_restaurante():
+  exibir_subtitulo("Cadastro de novos restaurantes")
+  nome_restaurante = input("Digite o nome do restaurante que deseja cadastrar: ")
+  restaurantes.append(nome_restaurante)
+  print(f"O restaurante {nome_restaurante} foi cadastrado com sucesso!")
+  voltar_ao_menu_principal()
+
+def listar_restaurantes():
+  exibir_subtitulo("Lista de Restaurantes")
+  for restaurante in restaurantes:
+    print(f".{restaurante}")
+  voltar_ao_menu_principal()
 
 def escolher_opcoes():
   try:
     opcao_escolhida = int(input("Escolha uma opção: "))
     print(f"Você escolheu a opção {opcao_escolhida}")
     if (opcao_escolhida == 1):
-      print("Cadastrar Restaurante")
+      cadastra_novo_restaurante()
     elif(opcao_escolhida == 2):
-      print("Cadastrar Restaurante")
+      listar_restaurantes()
     elif(opcao_escolhida == 3):
       print("Ativar Restaurante")
     elif(opcao_escolhida == 4):
       finalizar_app()
     else:
       opcao_invalida()
-  except ValueError:
+  except:
     opcao_invalida()
 
 def main():
